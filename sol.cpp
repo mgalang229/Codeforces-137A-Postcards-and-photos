9e1b8ab1ff148e3d612c33148ef4f55b56858d62
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-12-28 15:16:06 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+  string s;
+  cin >> s;
+  int n = (int) s.size();
+  int ans = 0;
+  // create segments with the same character (but only up to 5)
+  for (int i = 0; i < n; i++) {
+  	int j = i;
+  	int cnt = 0;
+  	while (s[i] == s[j] && j < n && cnt < 5) {
+  		cnt++;
+  		j++;
+  	}
+  	i = j - 1;
+  	ans++;
+  }
+  cout << ans << '\n';
+  return 0;
+}
